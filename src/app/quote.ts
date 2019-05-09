4db638e1eb431f74d2430b800ecd6dc52a051136
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-09 11:02:11 +0300

    fix bug on costructor in the quote.ts file

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-    constructor(public id:number, public name: string, public description: string, public publishDate:Date){
+    constructor(public id:number,public description: string, public name: string,  public publishDate:Date){
         this.showDescription = false
     }
 }
